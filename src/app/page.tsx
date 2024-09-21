4abbf38d33e869e692d7d9930a0d994cd1806a4c
aotatsu8 <70871669+aotatsu8@users.noreply.github.com>
2024-09-21 15:04:28 +0900

    pege.tsx修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,7 +37,7 @@ export default function Home() {
           height={37}
           priority
         /> */}
-        Hello World!!
+        Hello World
       </div>
 
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
